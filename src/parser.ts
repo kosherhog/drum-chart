71eef609781a3ad67685838c488929f4355a9345
jeffrey kay <jeff@jeffreykay.com>
2024-09-26 16:34:51 +0000

    Something that finally works

--- src/parser.ts
+++ src/parser.ts
@@ -1,35 +1,37 @@
 import { readFileSync } from 'fs';
 import { Token } from 'typescript-parsec';
 import { buildLexer, expectEOF, expectSingleResult, rule } from 'typescript-parsec';
-import { alt, apply, kmid, lrec_sc, rep, seq, str, tok } from 'typescript-parsec';
+import { alt, apply, kmid, lrec_sc, rep_sc, rep, seq, str, tok } from 'typescript-parsec';
 
 enum TokenKind {
-    Label,
-    Shortcut,
-    Value,
-    Blank
+    Directive,
+    Identifier,
+    String,
+    EOL,
 }
 
 const tokenizer = buildLexer([
-    [true, /^\S[^:]*/g, TokenKind.Label],
-    [true, /^(\S[^\r\n]*)\r?\n/g, TokenKind.Value],
-    [true, /^\S/g, TokenKind.Shortcut],
-    [true, /^\r?\n/g, TokenKind.Blank],
+    [true, /^\w[\w0-9]*/g, TokenKind.Identifier],
+    [true, /^:[^\r\n]+/g, TokenKind.String], // String are content between : and LF
+    [true, /^\r?\n/g, TokenKind.EOL],
+    [true, /^%\w+/g, TokenKind.Directive],
+    // [true, /^\S[^\r\n]*/g, TokenKind.Value],
 ]);
 
-function printme(value: any) {
-    console.log(`Value ${value}`);
+function printme(value: [ Token<TokenKind>, Token<TokenKind> ]) : string {
+    console.log(`\nValue ${value[0].text}`);
+    if (value[1].kind !== TokenKind.EOL) {
+        console.log(`\nString ${value[1].text}`);
+    }
+    return value[0].text;
 }
 
-const header = apply(str('%drum chart'),printme);
-const definition = seq(tok(TokenKind.Label),str(':'),tok(TokenKind.Shortcut));
-const def_block = rep(definition);
+const header = apply(seq(str<TokenKind>('%drumchart'),tok(TokenKind.EOL)),printme);
+const definition = seq(apply(seq(tok(TokenKind.Identifier),tok(TokenKind.String)),printme), tok(TokenKind.EOL));
+const def_block = seq(rep(definition),rep_sc(tok(TokenKind.EOL)));
 
-const entry = seq(tok(TokenKind.Shortcut),str(':'),tok(TokenKind.Value));
-const entry_block = rep(entry);
-
-const chart = seq(header,tok(TokenKind.Blank),def_block);
+const chart = seq(header,tok(TokenKind.EOL),def_block);
 
 
 const input = readFileSync('samples/verysmall.drum');
-const output = expectSingleResult(expectEOF(chart.parse(tokenizer.parse(input.toString()))));
+const output = expectSingleResult(expectEOF(chart.parse(tokenizer.parse(input.toString() + '\n'))));
