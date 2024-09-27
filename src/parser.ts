a67f8c86bf91038dbd74d41b0dc44e239fae4905
jeffrey kay <jeff@jeffreykay.com>
2024-09-27 01:04:38 +0000

    Working end to end for multiple definition blocks

--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,10 @@
 import { readFileSync } from 'fs';
-import { Token } from 'typescript-parsec';
+import { Token, TokenError } from 'typescript-parsec';
 import { buildLexer, expectEOF, expectSingleResult, rule } from 'typescript-parsec';
 import { alt, apply, kmid, lrec_sc, rep_sc, rep, seq, str, tok } from 'typescript-parsec';
 
-enum TokenKind {
+enum TokenKind
+{
     Directive,
     Identifier,
     String,
@@ -11,27 +12,35 @@ enum TokenKind {
 }
 
 const tokenizer = buildLexer([
-    [true, /^\w[\w0-9]*/g, TokenKind.Identifier],
-    [true, /^:[^\r\n]+/g, TokenKind.String], // String are content between : and LF
+    [true, /^[A-Za-z_]\w*/g, TokenKind.Identifier],
+    [true, /^:[^\r\n]+\r?\n/g, TokenKind.String], // String is content between : and LF
     [true, /^\r?\n/g, TokenKind.EOL],
     [true, /^%\w+/g, TokenKind.Directive],
-    // [true, /^\S[^\r\n]*/g, TokenKind.Value],
 ]);
 
-function printme(value: [ Token<TokenKind>, Token<TokenKind> ]) : string {
-    console.log(`\nValue ${value[0].text}`);
-    if (value[1].kind !== TokenKind.EOL) {
-        console.log(`\nString ${value[1].text}`);
+function printme(value: [Token<TokenKind>, Token<TokenKind>]): string
+{
+    console.log(`Value ${value[0].kind} ${value[0].text}`);
+    if (value[1].kind !== TokenKind.EOL)
+    {
+        console.log(`String ${value[1].kind} ${value[1].text}`);
     }
     return value[0].text;
 }
 
-const header = apply(seq(str<TokenKind>('%drumchart'),tok(TokenKind.EOL)),printme);
-const definition = seq(apply(seq(tok(TokenKind.Identifier),tok(TokenKind.String)),printme), tok(TokenKind.EOL));
-const def_block = seq(rep(definition),rep_sc(tok(TokenKind.EOL)));
+function printeol(value: Token<TokenKind>): string
+{
+    const next = value.next;
+    console.log(`Next: ${next?.kind} ${next?.text}`);
+    console.log(`EOL ${value.next}`);
+    return value.text;
+}
+const header = apply(seq(str<TokenKind>('%drumchart'), tok(TokenKind.EOL)), printme);
+const definition = apply(seq(tok(TokenKind.Identifier), tok(TokenKind.String)), printme);
+const def_block = seq(rep_sc(definition), rep_sc(apply(tok(TokenKind.EOL), printeol)));
 
-const chart = seq(header,tok(TokenKind.EOL),def_block);
-
+const chart = seq(header, rep_sc(tok(TokenKind.EOL)), rep_sc(def_block));
 
 const input = readFileSync('samples/verysmall.drum');
+
 const output = expectSingleResult(expectEOF(chart.parse(tokenizer.parse(input.toString() + '\n'))));
